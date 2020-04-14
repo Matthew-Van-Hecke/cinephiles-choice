0cefdf1018c784180af46a27831556d9150ab92b
Matthew VH <vanheckematt@gmail.com>
2020-04-14 17:02:16 -0500

    Built a service class for OMDb API in the Services folder. Built a method inside of this class called GetMovie which will take in a string for the query part of the needed url and return a JObject containing info on the movie.

---
+++ CinephilesChoiceAPI/CinephilesChoiceAPI/Services/OMDbAPI.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CinephilesChoiceAPI.Services
+{
+    public static class OMDbAPI
+    {
+        public static async Task<JObject> GetMovie(string movieQuery)
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(@"http://www.omdbapi.com/?apikey=" + movieQuery);
+                var data = await response.Content.ReadAsStringAsync();
+                JObject movie = JsonConvert.DeserializeObject<JObject>(data);
+                return movie;
+            }
+        }
+    }
+}
