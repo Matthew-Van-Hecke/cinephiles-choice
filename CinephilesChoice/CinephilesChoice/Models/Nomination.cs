ac79f9f51e129e3e11b11d202a24323622b1787c
Matthew VH <vanheckematt@gmail.com>
2020-04-16 15:34:45 -0500

    Created Nomination model, and NominationAPI class (with empty methods for GetAll, GetById, Create, Update, and Delete) in MVC app.

---
+++ CinephilesChoice/CinephilesChoice/Models/Nomination.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinephilesChoice.Models
+{
+    public class Nomination
+    {
+        public int Id { get; set; }
+        public string Nominee { get; set; }
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+        public string AwardCategory { get; set; }
+        public bool IsWinner { get; set; }
+        public string Year { get; set; }
+    }
+}
---
+++ CinephilesChoice/CinephilesChoice/Services/NominationAPI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CinephilesChoice.Models;
+
+namespace CinephilesChoice.Services
+{
+    public static class NominationAPI
+    {
+        public static async void GetAll()
+        {
+
+        }
+        public static async void GetById(int id)
+        {
+
+        }
+        public static async void Create(Nomination nomination)
+        {
+
+        }
+        public static async void Update(Nomination nomination)
+        {
+
+        }
+        public static async void Delete(Nomination nomination)
+        {
+
+        }
+    }
+}
