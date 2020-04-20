13268a589090079fa7a212c579d47120862e4964
Matthew VH <vanheckematt@gmail.com>
2020-04-20 11:32:15 -0500

    Created VoteViewModel which has, as its properties, a list of Nominations, and a Vote.

---
+++ CinephilesChoice/CinephilesChoice/Models/VoteViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinephilesChoice.Models
+{
+    public class VoteViewModel
+    {
+        public List<Nomination> Nominations { get; set; }
+        public Vote Vote { get; set; }
+    }
+}
