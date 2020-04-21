368213856e4d5c6b75b3bb5846a626bebc0b5eee
Matthew VH <vanheckematt@gmail.com>
2020-04-21 11:12:13 -0500

    Finished building CreateJsonVoteCollection method in JsonDataBuilder class.

--- CinephilesChoice/CinephilesChoice/Services/JsonDataBuilder.cs
+++ CinephilesChoice/CinephilesChoice/Services/JsonDataBuilder.cs
@@ -10,10 +10,11 @@ namespace CinephilesChoice.Services
 {
     public static class JsonDataBuilder
     {
-        public static string CreateJsonVoteCollection(List<Vote> votes)
+        public static string CreateJsonVoteCollection(NominationViewModel viewModel)
         {
-            var votesStringList = votes.SelectMany(,)
-            string votesAsJsonString = JsonConvert.SerializeObject(votes);
+            List<NomineeDateOfVoteViewModel> votesObjectList = viewModel.Votes.Select(v => new NomineeDateOfVoteViewModel(v.Date.Year, viewModel.Nominations.Where(n => n.Id == v.NominationId).First().Movie.Title)).ToList();
+            List<string> votesAsStringList = votesObjectList.Select(v => JsonConvert.SerializeObject(v)).ToList();
+            string votesAsJsonString = JsonConvert.SerializeObject(votesAsStringList);
             return votesAsJsonString;
         }
     }
