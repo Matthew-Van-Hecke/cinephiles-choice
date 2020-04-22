3f41b71a69c6864d6c30018c89f5b2cba73ff57c
Matthew VH <vanheckematt@gmail.com>
2020-04-22 07:30:18 -0500

    Moved some js content (helper functions and code that could be put in helper functions) from the script tag in DisplayNominations view to google-chart-helper-functions.js.

---
+++ CinephilesChoice/CinephilesChoice/wwwroot/js/google-chart-helper-functions.js
@@ -0,0 +1,18 @@
+﻿function createNomineesArray(nomineesJsonString) {
+    return JSON.parse(nomineesJsonString.replace(/&quot;/g, "\"").replace(/""/g, "\""));
+}
+
+function createVotesObjectArray(votesJsonString) {
+    return JSON.parse(votesJsonString.replace(/&quot;/g, "\"").replace(/""/g, "\"").replace(/"{/g, "{").replace(/}"/g, "}"));
+}
+
+function groupVotesByNominee(votesArray, nominees) {
+    let groupedArray = {};
+    for (let i = 0; i < nominees.length; i++) {
+        groupedArray[nominees[i]] = [];
+    }
+    for (let i = 0; i < votesArray.length; i++) {
+        groupedArray[votesArray[i]["Nominee"]].push(votesArray[i]);
+    }
+    return groupedArray;
+}
